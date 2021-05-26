3cabd643bb7972279c7ce940ad5bedbbc39e7e8e
vanduc19it <duc19it@gmail.com>
2021-05-26 08:35:37 +0700

    phung bedee

---
+++ app/src/main/java/com/example/foodrecipes/DangCongThuc.java
@@ -0,0 +1,14 @@
+package com.example.foodrecipes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DangCongThuc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_cong_thuc);
+    }
+}
